e85532519218abd0041275225233e227eb048d23
shanto <shantokumar22@gmail.com>
2026-05-17 22:18:16 +0600

    feat(category): display categories securely with HTML escaping and action links in show_list_category.php

--- show_list_category.php
+++ show_list_category.php
@@ -1,3 +1,13 @@
+<?php
+require_once("./sql_config.php");
+
+// লজিক উপরে রাখা ভালো
+$query = "SELECT * FROM category";
+$result = $conn->query($query);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,35 +18,35 @@
 </head>
 
 <body>
-    <table>
+    <?php if (isset($_GET['msg'])): ?>
+        <div class="msg-container">
+            <div class="msg">
+                <?php echo htmlspecialchars($_GET['msg']); ?>
+            </div>
+        </div>
+    <?php endif; ?>
+    <table border="1">
         <tr class="header">
             <th>Category-ID</th>
             <th>Category_Name</th>
-            <th>Category_Entrydate</th>
+            <th colspan="2">Category_Entrydate</th>
         </tr>
 
-
         <?php
-        require_once("./sql_config.php");
-
-
-        $query = "SELECT * FROM category";
-
-        $result = $conn->query($query);
-
-
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
 
         ?>
                 <tr>
-                    <td><?php echo $row['category_id'] ?></td>
-                    <td><?php echo $row['category_name'] ?></td>
-                    <td><?php echo $row['category_entrydate'] ?></td>
+                    <td><?php echo htmlspecialchars($row['category_id']) ?></td>
+                    <td><?php echo htmlspecialchars($row['category_name']) ?></td>
+                    <td><?php echo htmlspecialchars($row['category_entrydate']) ?></td>
+                    
+                    <td><?php echo htmlspecialchars($row['category_entrydate']) ?></td>
 
-                    <td>
-                        <a href="edit.php?id=<?php $row['category_id'] ?>">EDIT</a>
-                    </td>
+                    <td><a href="./edit_category.php?id=<?php echo urlencode($row['category_id']) ?>">Edit</a></td>
+                    
+                    <td><a href="./delete_category.php?id=<?php echo urlencode($row['category_id']) ?>" onclick="return confirm('Delete this category?')">Delete</a></td>
 
                 </tr>
 
