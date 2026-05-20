f6658fa3bc6e5ba602133fac52726e8c825a52f4
shanto <shantokumar22@gmail.com>
2026-05-20 08:26:09 +0600

    refactor: clean up the show list category layout and secure query result loops in show_list_category.php

--- show_list_category.php
+++ show_list_category.php
@@ -1,67 +1,53 @@
-<?php
-require_once("./sql_config.php");
-
-// লজিক উপরে রাখা ভালো
-$query = "SELECT * FROM category";
-$result = $conn->query($query);
-
+<?php 
+    require_once('./sql_config.php');
+    $query = "SELECT * FROM category";
+    $result = $conn->query($query);
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>show list category</title>
+    <title>Document</title>
 </head>
-
 <body>
-    <?php if (isset($_GET['msg'])): ?>
-        <div class="msg-container">
-            <div class="msg">
-                <?php echo htmlspecialchars($_GET['msg']); ?>
-            </div>
+    <?php 
+        if(!empty($_GET['msg'])){
+    ?>
+        <div>
+            <?php echo htmlspecialchars($_GET['msg'])?>
         </div>
-    <?php endif; ?>
+
+    <?php
+       }
+    ?> 
+
     <table border="1">
-        <tr class="header">
-            <th>Category-ID</th>
-            <th>Category_Name</th>
-            <th colspan="2">Category_Entrydate</th>
+        <tr>
+            <th>category_id</th>
+            <th>category_name</th>
+            <th>category_entrydate</th>
+            <th colspan="2">Action</th>
         </tr>
-
-        <?php
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-
+        <?php 
+            if($result->num_rows > 0){
+                while($row = $result->fetch_assoc()){
         ?>
-                <tr>
-                    <td><?php echo htmlspecialchars($row['category_id']) ?></td>
-                    <td><?php echo htmlspecialchars($row['category_name']) ?></td>
-                    <td><?php echo htmlspecialchars($row['category_entrydate']) ?></td>
-                    
-                    <td><?php echo htmlspecialchars($row['category_entrydate']) ?></td>
 
-                    <td><a href="./edit_category.php?id=<?php echo urlencode($row['category_id']) ?>">Edit</a></td>
-                    
-                    <td><a href="./delete_category.php?id=<?php echo urlencode($row['category_id']) ?>" onclick="return confirm('Delete this category?')">Delete</a></td>
+        <tr>
+            <td><?php echo htmlspecialchars($row['category_id']) ?></td>
+            <td><?php echo htmlspecialchars($row['category_name']) ?></td>
+            <td><?php echo htmlspecialchars($row['category_entrydate']) ?></td>
+            <td><a href="edit_category.php?id=<?php echo htmlspecialchars($row['category_id']) ?>">EDIT</a></td>
+            <td><a href="delete_category.php?id=<?php echo htmlspecialchars($row['category_id']) ?>" onclick="return confirm('Delete this Category?')">Delete</a></td>
 
-                </tr>
-
+        </tr>
         <?php
+                }
             }
-        }
         ?>
 
     </table>
-
-
-
-
-
-
 </body>
-
 </html>
